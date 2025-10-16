d88e3dd0999fa04b391289a2b84cf0b47f4f457a
Nishit Prajapat <120020921+nishitprajapat@users.noreply.github.com>
2025-10-16 11:29:55 +0530

    Add files via upload

---
+++ Logguard-mongo/backend/readme.md
@@ -0,0 +1,5 @@
+// backend -->
+ 
+npm init -y
+npm install express@4 
+node --watch server.js
---
+++ Logguard-mongo/backend/server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { MongoClient } = require('mongodb');
+const bodyparser  = require('body-parser')
+const cors = require('cors')
+
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+client.connect();
+
+const dbName = 'loggaurd';
+const app = express()
+const port = 3000
+
+
+app.use(bodyparser.json())
+app.use(cors())
+
+
+//Get all the password
+app.get('/', async (req, res) => {
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult)
+  
+})
+
+//Save a password
+app.post('/', async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.insertOne(password);
+  res.send({success: true, result: findResult})
+  
+})
+
+//Delete password
+app.delete('/', async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection('passwords');
+  const findResult = await collection.deleteOne(password);
+  res.send({success: true, result: findResult})
+  
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
